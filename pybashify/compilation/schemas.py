1961bbc0075cdb393b27c0c7deb1d4a058b4eff9
Ameer Arsala <ameer.arsala03@gmail.com>
2024-10-14 13:34:59 -0700

    formatting

--- pybashify/compilation/schemas.py
+++ pybashify/compilation/schemas.py
@@ -1,6 +1,6 @@
-#from typing import Optional
+# from typing import Optional
 
-from pydantic import BaseModel #, Field
+from pydantic import BaseModel  # , Field
 
 
 class BashifyDeclaration(BaseModel):
